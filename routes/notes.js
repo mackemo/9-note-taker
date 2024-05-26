2d7b44af45549bc4c8013e3d0018d6e34d51a168
mackemo <mackenzielmoore14@gmail.com>
2024-05-26 14:17:30 -0400

    added basic outline for get, post, and delete routes in notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,15 @@
 const router = require('express').Router();
+
+router.get('/', (req, res) => {
+
+})
+
+
+router.post('/', (req, res) => {
+    
+})
+
+
+router.delete('/', (req, res) => {
+    
+})
