0a4af3b2817338e39d7f94f5eeb6a01e717f8fd3
mackemo <mackenzielmoore14@gmail.com>
2024-05-27 15:49:06 -0400

    added logic to post route, returns error if theres no title or text, reads db.json file, then using try/catch, parses data, creates new notes that is pushed to parsed data, write new file in db.json with new note

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 
 // all paths for notes router (get, post, delete)
@@ -17,17 +18,52 @@ router.get('/', (req, res) => {
             console.error('Error parsing db.json:', err);
             res.status(500).json({error: 'Internal Server Error'});
         }
-    })
-})
+    });
+});
 
 
 router.post('/', (req, res) => {
+    const { title, text } = req.body;
+    if (!title || !text) {
+        return res.status(400).json({error: 'Please include both a title and text for the note.'});
+    }
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading db.json:', err);
+            return res.status(500).json({error: 'Internal Server Error'});
+        }
+
+        try {
+            const notes = JSON.parse(data);
     
-})
+            const newNote = {
+                id: uuidv4(),
+                title,
+                text
+            };
+    
+            notes.push(newNote);
+    
+            fs.writeFile('./db/db.json', JSON.stringify(notes, null, '\t'), (err) => {
+                if (err) {
+                    console.error('Error writing to db.json:', err);
+                    return res.status(500).json({error: 'Internal Server Error'});
+                }
+    
+                res.status(201).json(newNote);
+            });
+    
+        } catch (err) {
+            console.error('Error parsing db.json:', err);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
+    });
+});
 
 
 router.delete('/', (req, res) => {
     
-})
+});
 
 module.exports = router;
