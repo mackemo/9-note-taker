9f322af85cc196c1e80ed4020ec3bc822683fda7
mackemo <mackenzielmoore14@gmail.com>
2024-05-27 15:17:43 -0400

    added get route logic, read the db file, return errors if error, parse the data and json data as response, using try/catch for response

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const fs = require('fs');
 
+
+// all paths for notes router (get, post, delete)
 router.get('/', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading db.json:', err);
+            return res.status(500).json({error: 'Internal Server Error'});
+        }
 
+        try {
+            const notes = JSON.parse(data);
+            res.json(notes);
+        } catch (err) {
+            console.error('Error parsing db.json:', err);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
+    })
 })
 
 
@@ -13,3 +29,5 @@ router.post('/', (req, res) => {
 router.delete('/', (req, res) => {
     
 })
+
+module.exports = router;
