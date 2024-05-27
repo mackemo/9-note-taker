be264358cf8ab2b5420a955974321e9e6d069a96
mackemo <mackenzielmoore14@gmail.com>
2024-05-27 14:50:51 -0400

    commenting on index.js in routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
+// importing the router
 const notesRouter = require('./notes');
 
+// using the router
 router.use('/notes', notesRouter);
 
 module.exports = router;
